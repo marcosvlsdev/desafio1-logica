7e82fdf36d69f9b051001a6a8566e9803405681e
Marcos Vinicius <158005194+marcosvlsdev@users.noreply.github.com>
2025-06-18 21:04:28 -0300

    test: add test with only if and else if

---
+++ index.js
@@ -0,0 +1,41 @@
+let nome = "Markola Capudo"
+let xp = 2005
+let rank = ""
+
+if(xp<1000) {
+    rank = "Ferro"
+}
+
+else if(xp>=1000 && xp<2000) {
+    rank = "Bronze"
+}
+
+else if(xp>=2000 && xp<3000) {
+    rank = "Prata"
+}
+
+else if(xp>=3000 && xp<4000) {
+    rank = "Ouro"
+}
+
+else if(xp>=5000 && xp<6000) {
+    rank = "Diamante"
+}
+
+else if(xp>=6000 && xp<7000) {
+    rank = "Platina"
+}
+
+else if(xp>=7000 && xp<8000) {
+    rank = "Ascendente"
+}
+
+else if(xp>=9000 && xp<10000) {
+    rank = "Imortal"
+}
+
+else if(xp>10000) {
+    rank = "Radiante"
+}
+
+console.log("O herói " + nome + " está no rank " + rank)
